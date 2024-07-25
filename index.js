4ecf875cba74b0d247c46cc937f321a7760efac7
José López <jslquintero@proton.me>
2024-07-25 16:16:41 -0500

    correccion en los pills

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ function SearchBox({
         </div>
       </div>
       <div className="grid grid-cols-1 my-2">
-        <div className="grid grid-cols-4 gap-2">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
           <FilterBadge
             label="Location"
             value={filters.location?.city ? filters.location.city : ""}
